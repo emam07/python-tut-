1d6212fee88576e2afea60501ab38359b1e6e09d
emam09 <emamulkhanmgs@gmail.com>
2025-07-01 12:34:00 +0530

    commit

--- pp1.py
+++ pp1.py
@@ -1,1 +1,18 @@
 print("HELLO WORLD")
+'''
+this is the python comment which i am doing while learning this is multiline comment
+
+'''
+
+
+name="harijan"
+num=78656
+print(num)
+
+is_raining=True
+is_notraining=False
+print(name)
+print (is_raining)
+
+print(type(is_notraining))
+
